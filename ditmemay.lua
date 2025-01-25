ba3b5441d7f3684a858872673581642af910791f
nhannguyen124 <133207402+nhannguyen124@users.noreply.github.com>
2025-01-25 14:53:25 +0700

    Update ditmemay.lua

--- ditmemay.lua
+++ ditmemay.lua
@@ -1,35 +1,5 @@
-return function(Settings) 
-    local data = Settings[2]
-    local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-    
-    -- Xóa các ký tự không hợp lệ khỏi chuỗi base64
-    data = string.gsub(data, '[^' .. b .. '=]', '')
-
-    -- Hàm giải mã base64
-    local decodedData = (data:gsub('.', function(x)
-        if (x == '=') then return '' end
-        local r, f = '', (b:find(x) - 1)
-        
-        for i = 6, 1, -1 do
-            r = r .. (f % 2^i - f % 2^(i - 1) > 0 and '1' or '0')
-        end
-        
-        return r
-    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
-        if (#x ~= 8) then return '' end
-        local c = 0
-        
-        for i = 1, 8 do
-            c = c + (x:sub(i, i) == '1' and 2^(8 - i) or 0)
-        end
-        
-        return string.char(c)
-    end))
-
-    -- Dịch chuyển người chơi đến server cụ thể
-    game:GetService("TeleportService"):TeleportToPlaceInstance(
-        Settings[1], -- PlaceId
-        decodedData, -- Decoded JobId
-        game.Players.LocalPlayer -- Người chơi
-    )
+return function(Settings)
+  local decodeBase64 = loadstring(game:HttpGet("https://raw.githubusercontent.com/nhannguyen124/nhannguyen124/refs/heads/main/base64decoded.lua"))
+  local decodedData = decodeBase64(Settings[2])
+  game:GetService("TeleportService"):TeleportToPlaceInstance(Settings[1], decodedData, game.Players.LocalPlayer)
 end

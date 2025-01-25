37ce46f5a90426c0b0f4944b13fd8213c83fd03a
nhannguyen124 <133207402+nhannguyen124@users.noreply.github.com>
2025-01-25 15:00:04 +0700

    Update base64decoded.lua

--- base64decoded.lua
+++ base64decoded.lua
@@ -1,29 +1,30 @@
 return function(data)
     local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-    
-    -- Loại bỏ ký tự không hợp lệ khỏi chuỗi Base64
-    data = string.gsub(data, '[^' .. b .. '=]', '')
 
-    -- Giải mã Base64
+    -- Remove invalid Base64 characters
+    data = data:gsub('[^' .. b .. '=]', '')
+
+    -- Decode Base64
     local decoded = data:gsub('.', function(x)
         if x == '=' then return '' end
-        local r, f = '', (b:find(x) - 1)
+        local f = (b:find(x) or 1) - 1
+        local r = ''
         
-        -- Chuyển ký tự thành chuỗi bit
+        -- Convert character to binary string
         for i = 6, 1, -1 do
-            r = r .. (f % 2^i - f % 2^(i - 1) > 0 and '1' or '0')
+            r = r .. ((f % 2^i - f % 2^(i - 1)) > 0 and '1' or '0')
         end
-        
+
         return r
     end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(bits)
         if #bits ~= 8 then return '' end
         local char = 0
         
-        -- Chuyển chuỗi bit thành ký tự
+        -- Convert binary string to character
         for i = 1, 8 do
-            char = char + (bits:sub(i, i) == '1' and 2^(8 - i) or 0)
+            char = char + ((bits:sub(i, i) == '1') and 2^(8 - i) or 0)
         end
-        
+
         return string.char(char)
     end)
 

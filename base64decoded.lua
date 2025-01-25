a219eff1fabfcc475ac7d6a208a0df1029db25a6
nhannguyen124 <133207402+nhannguyen124@users.noreply.github.com>
2025-01-25 15:59:24 +0700

    Update base64decoded.lua

--- base64decoded.lua
+++ base64decoded.lua
@@ -1,17 +1,20 @@
-return function(data)
-    local function decodedbase64(data)
-        return (data:gsub('.', function(x)
-            local r, b = '', x:byte()
-            for i = 8, 1, -1 do
-                r = r .. (b % 2^i - b % 2^(i-1) > 0 and '1' or '0')
-            end
-            return r
-        end):gsub('%z', ''):gsub('(%d%d%d?)(%d%d?)(%d?)', function(a, b, c)
-            local r = tonumber(a, 2) or 0
-            r = r * 64 + (tonumber(b, 2) or 0)
-            r = r * 4 + (tonumber(c, 2) or 0)
-            return string.char(r)
-        end))
-    end
+local function decodedbase64(data)
+    return (data:gsub('.', function(x)
+        local r, b = '', x:byte()
+        for i = 8, 1, -1 do
+            r = r .. (b % 2^i - b % 2^(i-1) > 0 and '1' or '0')
+        end
+        return r
+    end):gsub('%z', ''):gsub('(%d%d%d?)(%d%d?)(%d?)', function(a, b, c)
+        local r = tonumber(a, 2) or 0
+        r = r * 64 + (tonumber(b, 2) or 0)
+        r = r * 4 + (tonumber(c, 2) or 0)
+        return string.char(r)
+    end))
+end
+
+local function decode(data)
     return decodedbase64(data)
 end
+
+return decode

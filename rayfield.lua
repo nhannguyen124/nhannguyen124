0d8ec6c77fa451c7f823e9402fbf6f8dbf45c91e
nhannguyen124 <133207402+nhannguyen124@users.noreply.github.com>
2026-03-05 15:38:08 +0700

    Simplify SliderSettings.Range assignment

--- rayfield.lua
+++ rayfield.lua
@@ -3328,9 +3328,7 @@ function RayfieldLibrary:CreateWindow(Settings)
 
 		-- Slider
 		function Tab:CreateSlider(SliderSettings)
-			if not SliderSettings.Range then
-				SliderSettings.Range = {SliderSettings.Min,SliderSettings.Max}
-			end
+			SliderSettings.Range = SliderSettings.Range or {SliderSettings.Min, SliderSettings.Max}
 			local SLDragging = false
 			local Slider = Elements.Template.Slider:Clone()
 			Slider.Name = SliderSettings.Name
